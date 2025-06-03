48549be95bb566c361ddeff30a15875c9aa96091
Abubakr111 <gbolahanbakry111@gmail.com>
2025-06-03 18:02:07 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const joke = document.querySelector('.joke');
+const jokeBtn = document.querySelector('.btn');
+
+
+generateJoke = () => {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 ) {
+        const data = JSON.parse(this.responseText);
+
+        joke.innerHTML = data.value;
+        }
+    }
+
+    xhr.send();
+}
+
+
+jokeBtn.addEventListener('click', generateJoke);
